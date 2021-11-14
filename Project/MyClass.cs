5e18a377a15d3ac0c169944a1ea18f4b82143f3a
fbnhyn <fabian.heyne@outlook.com>
2021-11-14 15:00:44 +0100

    Add project files.

---
+++ Project/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class MyClass
+    {
+        public string Name => "My ILRepacked Class";
+    }
+}
